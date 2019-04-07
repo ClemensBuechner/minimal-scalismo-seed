294ce038d7c85b4a1ae5e77e9dca56638352059d
clemens.buechner@unibas.ch <clemens.buechner@unibas.ch>
2019-04-07 12:20:04 +0200

    added warp to approach target mesh with reference, run RigidAlignment first to get aligned landmarks

--- src/main/scala/ex1/RigidAlignment.scala
+++ src/main/scala/ex1/RigidAlignment.scala
@@ -6,8 +6,12 @@ import java.io.File
 import scalismo.geometry._
 import scalismo.io.{LandmarkIO, MeshIO}
 import scalismo.mesh.TriangleMesh
-import scalismo.registration.{LandmarkRegistration, RigidTransformation, RotationTransform, TranslationTransform}
+import scalismo.registration.{
+  LandmarkRegistration, RigidTransformation, RotationTransform,
+  TranslationTransform
+}
 import scalismo.ui.api.ScalismoUI
+import scalismo.utils.Random
 
 
 object RigidAlignment {
@@ -15,26 +19,38 @@ object RigidAlignment {
   def main(args: Array[String]) {
 
     scalismo.initialize()
-    implicit val rng = scalismo.utils.Random(42)
+    implicit val rng: Random = scalismo.utils.Random(42)
 
     val ui = ScalismoUI()
 
     val femoraGroup = ui.createGroup("femora")
 
     val refMesh: TriangleMesh[_3D] = MeshIO.readMesh(new java.io.File("datasets/femur.stl")).get
-    val refLM = LandmarkIO.readLandmarksJson[_3D](new File("datasets/femur.json")).get
+    val refLM: Seq[Landmark[_3D]] = LandmarkIO.readLandmarksJson[_3D](new File("datasets/femur" +
+      ".json")).get
 
     val refMeshView = ui.show(femoraGroup, refMesh, "Reference Femur")
     refMeshView.color = Color.ORANGE
 
 
     for (x <- 0 to 49) {
-      val femurMesh: TriangleMesh[_3D] = MeshIO.readMesh(new File("data/femora/meshes/" + x + ".stl")).get
-      val femurLM = LandmarkIO.readLandmarksJson[_3D](new File("data/femora/landmarks/" + x + ".json")).get
+      val femurMesh: TriangleMesh[_3D] = MeshIO.readMesh(new File("data/femora/meshes/" + x + "" +
+        ".stl")).get
+      val femurLM = LandmarkIO.readLandmarksJson[_3D](new File("data/femora/landmarks/" + x + "" +
+        ".json")).get
+      val lmIds = femurLM.map { lm => femurMesh.pointSet.findClosestPoint(lm.point).id }
 
-      val bestTransform: RigidTransformation[_3D] = LandmarkRegistration.rigid3DLandmarkRegistration(femurLM, refLM, center = Point(0, 0, 0))
+      val bestTransform: RigidTransformation[_3D] = LandmarkRegistration
+        .rigid3DLandmarkRegistration(femurLM, refLM, center = Point(0, 0, 0))
       val alignedFemur = femurMesh.transform(bestTransform)
+      val alignedLM: Seq[Landmark[_3D]] = lmIds.map { id =>
+        Landmark(id.toString, alignedFemur
+          .pointSet.point(id))
+      }
+
       MeshIO.writeMesh(alignedFemur, new File("data/femora/aligned/" + x + ".stl"))
+      LandmarkIO.writeLandmarksJson[_3D](alignedLM, new File("data/femora/alignedLandmarks/" + x
+        + ".json"))
 
       val femurMeshView = ui.show(femoraGroup, alignedFemur, "Femur" + x)
     }
--- src/main/scala/femurProject/FemurReconstruction.scala
+++ src/main/scala/femurProject/FemurReconstruction.scala
@@ -2,14 +2,12 @@ package femurProject
 
 import java.io.File
 
-import breeze.interpolation.LinearInterpolator
-import org.apache.commons.math3.analysis.interpolation.FieldHermiteInterpolator
-import scalismo.common.{DiscreteField, NearestNeighborInterpolator, PointId, UnstructuredPointsDomain}
+import scalismo.common._
 import scalismo.geometry.{EuclideanVector, Landmark, Point, _3D}
 import scalismo.io.{LandmarkIO, MeshIO}
-import scalismo.mesh.TriangleMesh
+import scalismo.mesh.{TriangleMesh, TriangleMesh3D}
 import scalismo.registration.LandmarkRegistration
-import scalismo.statisticalmodel.{DiscreteLowRankGaussianProcess, GaussianProcess, StatisticalMeshModel}
+import scalismo.statisticalmodel.{DiscreteLowRankGaussianProcess, StatisticalMeshModel}
 import scalismo.ui.api.ScalismoUI
 import scalismo.utils.Random
 
@@ -21,48 +19,57 @@ object FemurReconstruction {
     scalismo.initialize()
     val ui = ScalismoUI()
 
+    val ref = MeshIO.readMesh(new File("datasets/femur.stl")).get
+    val refLMs = LandmarkIO.readLandmarksJson[_3D](new File("datasets/femur.json")).get
+    val refLMpts = landmarksToPoints(refLMs)
+
     val files = new File("data/femora/aligned/").listFiles()
     val dataset = files.map { f => MeshIO.readMesh(f).get }
-    val lmsFiles = new File("data/femora/landmarks/").listFiles()
-    val lms: Array[Seq[Landmark[_3D]]] = files.map { f => LandmarkIO.readLandmarksJson[_3D](f).get }
+    val lmsFiles = new File("data/femora/alignedLandmarks/").listFiles()
+    val lms = lmsFiles.map { f => LandmarkIO.readLandmarksJson[_3D](f).get }
 
-    val reference = dataset.head
-    val refLm: Seq[Landmark[_3D]] = lms.head
-    val refLmPts: IndexedSeq[Point[_3D]] = refLm.map { lm => lm.point }.toIndexedSeq
 
-    val defFields = (1 until dataset.length).map { i: Int =>
-      val gp = getGPfromLandmarks(refLmPts, refLm, lms(i))
-      val mesh: TriangleMesh[_3D] = dataset(i).pointSet.pointIds.map { id: PointId =>
-        gp.marginal(reference.pointSet)
-      }
+    val defFields = (0 until 5 /*dataset.length*/).map { i: Int =>
+      val mesh = dataset(i)
+      val warpedRef = warpMesh(ref, refLMpts, landmarksToPoints(lms(i)))
+//      ui.show(dataset(i), "femora" + i)
+//      ui.show(warpedRef, "warp" + i)
 
-      val ids = (0 until m.pointSet.numberOfPoints by 50).map(i => PointId(i))
-      val aligned: TriangleMesh[_3D] = ICPRigidAlign(m, reference, ids, 150)
+      val ids = (0 until mesh.pointSet.numberOfPoints by 50).map(i => PointId(i))
+      val aligned: TriangleMesh[_3D] = ICPRigidAlign(mesh, ref, ids, 150)
       val deformationVectors = aligned.pointSet.pointIds.map { id: PointId =>
-        val p = m.pointSet.point(id)
-        p - reference.pointSet.findClosestPoint(p).point
+        val p = mesh.pointSet.point(id)
+        p - ref.pointSet.findClosestPoint(p).point
       }.toIndexedSeq
 
       println("finished alignment")
-      DiscreteField[_3D, UnstructuredPointsDomain[_3D], EuclideanVector[_3D]](m.pointSet,
+      DiscreteField[_3D, UnstructuredPointsDomain[_3D], EuclideanVector[_3D]](mesh.pointSet,
         deformationVectors)
     }
 
     val interpolator = NearestNeighborInterpolator[_3D, EuclideanVector[_3D]]()
     val contiuousField = defFields.map(f => f.interpolate(interpolator))
-    val gp1 = DiscreteLowRankGaussianProcess.createUsingPCA(reference.pointSet, contiuousField)
-    val model1 = StatisticalMeshModel(reference, gp1.interpolate(interpolator))
+    val gp = DiscreteLowRankGaussianProcess.createUsingPCA(ref.pointSet,
+      contiuousField)
+    val model1 = StatisticalMeshModel(ref, gp.interpolate(interpolator))
     ui.show(model1, "mean")
   }
 
-  def getGPfromLandmarks(reference: IndexedSeq[Point[_3D]], refLms: Seq[Landmark[_3D]],
-                         lms: Seq[Landmark[_3D]]):
-  DiscreteLowRankGaussianProcess[_3D, IndexedSeq[Point[_3D]], EuclideanVector[_3D]] = {
-    val defField = lms.indices.map { i: Int => refLms(i).point - lms(i).point }
-    val discField: DiscreteField[_3D, UnstructuredPointsDomain[_3D], EuclideanVector[_3D]] =
-      DiscreteField(UnstructuredPointsDomain(reference), defField)
-    DiscreteLowRankGaussianProcess.createUsingPCA(reference,
-      Seq(discField.interpolate(NearestNeighborInterpolator()))) // TODO: replace nearestNeighbourInterpolator
+  def landmarksToPoints(lms: Seq[Landmark[_3D]]): IndexedSeq[Point[_3D]] = {
+    lms.map { lm => lm.point }.toIndexedSeq
+  }
+
+  def warpMesh(mesh: TriangleMesh[_3D], orig: IndexedSeq[Point[_3D]],
+               target: IndexedSeq[Point[_3D]]): TriangleMesh3D = {
+    val vectors = orig.indices.map { i: Int => target(i) - orig(i) }
+    val warpedPts = mesh.pointSet.points.map { p =>
+      val dists = orig.map { o => (p - o).norm2 }
+      val distSum = dists.sum
+      val weights = dists.map { d => d / distSum }
+      val weightedVecs = vectors.indices.map { i: Int => vectors(i) * weights(i) }
+      p + weightedVecs.reduce { (a, b) => a + b }
+    }.toIndexedSeq
+    TriangleMesh3D(warpedPts, mesh.triangulation)
   }
 
   def attributeCorrespondences(movingMesh: TriangleMesh[_3D], staticMesh: TriangleMesh[_3D],
