d3f96bb8006ff23eac8787be3070e3b3af897e42
clemens.buechner@unibas.ch <clemens.buechner@unibas.ch>
2019-05-17 08:35:36 +0200

    Second try, routine based on tutorial 15 file.

--- src/main/scala/segmentationProject/FemurSegmentation.scala
+++ src/main/scala/segmentationProject/FemurSegmentation.scala
@@ -1,14 +1,16 @@
 package segmentationProject
 
+import java.awt.Color
 import java.io.File
 
 import breeze.linalg.DenseVector
 import scalismo.common.PointId
 import scalismo.geometry.{EuclideanVector, Point3D, _3D}
 import scalismo.io.{ActiveShapeModelIO, ImageIO, MeshIO, StatisticalModelIO}
+import scalismo.mesh.{TriangleMesh, TriangleMesh3D}
 import scalismo.registration.{RigidTransformation, RotationTransform, TranslationTransform}
 import scalismo.sampling.DistributionEvaluator
-import scalismo.statisticalmodel.asm.{FittingConfiguration, ModelTransformations, NormalDirectionSearchPointSampler}
+import scalismo.statisticalmodel.asm._
 import scalismo.statisticalmodel.{MultivariateNormalDistribution, StatisticalMeshModel}
 import scalismo.ui.api.ScalismoUI
 import scalismo.utils.Memoize
@@ -46,7 +48,14 @@ object FemurSegmentation {
     tests.foreach { i: Int =>
       val image = ImageIO.read3DScalarImage[Short](new File(dataDir + "test/" + i + ".nii")).get
         .map(_.toFloat)
-      val reference = MeshIO.readMesh(new File(dataDir + "test/" + i + ".stl"))
+
+      val reference = MeshIO.readMesh(new File(dataDir + "test/" + i + ".stl")).get
+
+      val testGroup = ui.createGroup("test_" + i)
+      val imgView = ui.show(testGroup, image, "image_" + i)
+      val refView = ui.show(testGroup, reference, "reference")
+      refView.color = Color.GREEN
+
       val preprocessedImage = asm.preprocessor(image)
 
       val modelBoundingBox = asm.statisticalModel.referenceMesh.boundingBox
@@ -54,23 +63,33 @@ object FemurSegmentation {
 
       val translationTransformation = TranslationTransform(EuclideanVector(0, 0, 0))
       val rotationTransformation = RotationTransform(0, 0, 0, rotationCenter)
-      val initialRigidTransformation = RigidTransformation(translationTransformation, rotationTransformation)
+      val initialRigidTransformation = RigidTransformation(translationTransformation,
+        rotationTransformation)
       val initialModelCoefficients = DenseVector.zeros[Double](asm.statisticalModel.rank)
-      val initialTransformation = ModelTransformations(initialModelCoefficients, initialRigidTransformation)
+      val initialTransformation = ModelTransformations(initialModelCoefficients,
+        initialRigidTransformation)
 
       val numberOfIterations = 20
-      val asmIterator = asm.fitIterator(image, searchPointSampler, numberOfIterations, config, initialTransformation)
+      val asmIterator = asm.fitIterator(image, searchPointSampler, numberOfIterations, config,
+        initialTransformation)
 
       val asmIteratorWithVisualization = asmIterator.map(it => {
         it match {
           case scala.util.Success(iterationResult) => {
-            modelView.shapeModelTransformationView.poseTransformationView.transformation = iterationResult.transformations.rigidTransform
-            modelView.shapeModelTransformationView.shapeTransformationView.coefficients = iterationResult.transformations.coefficients
+            modelView.shapeModelTransformationView.poseTransformationView.transformation =
+              iterationResult.transformations.rigidTransform
+            modelView.shapeModelTransformationView.shapeTransformationView.coefficients =
+              iterationResult.transformations.coefficients
           }
           case scala.util.Failure(error) => System.out.println(error.getMessage)
         }
         it
       })
+
+      val result = asmIteratorWithVisualization.toIndexedSeq.last
+      val finalMesh = result.get.mesh
+
+      val testView = ui.show(testGroup, finalMesh, "testMesh")
     }
 
     //    val targetCTs = targets.map { i: Int =>
@@ -124,74 +143,94 @@ case class PriorEvaluator(model: StatisticalMeshModel) extends DistributionEvalu
   }
 }
 
-case class SimpleCorrespondenceEvaluator(model: StatisticalMeshModel, correspondences: Seq[
-  (PointId, Point3D, MultivariateNormalDistribution)]) extends DistributionEvaluator[Sample] {
+case class ActiveShapeModelEvaluator(model: StatisticalMeshModel, asm: ActiveShapeModel,
+                                     mesh: TriangleMesh[_3D],
+                                     preprocessedImage: PreprocessedImage) extends
+  DistributionEvaluator[Sample] {
 
   override def logValue(sample: Sample): Double = {
 
-    val currModelInstance = model.instance(sample.parameters.modelCoefficients).transform(sample
-      .poseTransformation)
+      val ids = asm.profiles.ids
 
-    val likelihoods = correspondences.map(correspondence => {
-      val (id, targetPoint, uncertainty) = correspondence
-      val modelInstancePoint = currModelInstance.pointSet.point(id)
-      val observedDeformation = targetPoint - modelInstancePoint
-
-      uncertainty.logpdf(observedDeformation.toBreezeVector)
-    })
-
-
-    val loglikelihood = likelihoods.sum
-    loglikelihood
-  }
-}
-
-case class CorrespondenceEvaluator(model: StatisticalMeshModel, correspondences: Seq[(PointId,
-  Point3D, MultivariateNormalDistribution)]) extends DistributionEvaluator[Sample] {
-
-  val (marginalizedModel, newCorrespondences) = marginalizeModelForCorrespondences(model,
-    correspondences)
-
-  override def logValue(sample: Sample): Double = {
-
-    val currModelInstance = marginalizedModel.instance(sample.parameters.modelCoefficients)
-      .transform(sample.poseTransformation)
-
-    val likelihoods = newCorrespondences.map(correspondence => {
-      val (id, targetPoint, uncertainty) = correspondence
-      val modelInstancePoint = currModelInstance.pointSet.point(id)
-      val observedDeformation = targetPoint - modelInstancePoint
-
-      uncertainty.logpdf(observedDeformation.toBreezeVector)
-    })
-
-
-    val loglikelihood = likelihoods.sum
-    loglikelihood
+      val likelihoods = for (id <- ids) yield {
+        val profile = asm.profiles(id)
+        val profilePointOnMesh = mesh.pointSet.point(profile.pointId)
+        val featureAtPoint = asm.featureExtractor(preprocessedImage, profilePointOnMesh, mesh,
+          profile.pointId).get
+        profile.distribution.logpdf(featureAtPoint)
+      }
+      likelihoods.sum
+    }
   }
 
-  def marginalizeModelForCorrespondences(model: StatisticalMeshModel, correspondences: Seq[
-    (PointId, Point3D, MultivariateNormalDistribution)]): (StatisticalMeshModel, Seq[(PointId,
-    Point3D, MultivariateNormalDistribution)]) = {
+  case class SimpleCorrespondenceEvaluator(model: StatisticalMeshModel, correspondences: Seq[
+    (PointId, Point3D, MultivariateNormalDistribution)]) extends DistributionEvaluator[Sample] {
 
-    val (modelIds, _, _) = correspondences.unzip3
-    val marginalizedModel = model.marginal(modelIds.toIndexedSeq)
-    val newCorrespondences = correspondences.map(idWithTargetPoint => {
-      val (id, targetPoint, uncertainty) = idWithTargetPoint
-      val modelPoint = model.referenceMesh.pointSet.point(id)
-      val newId = marginalizedModel.referenceMesh.pointSet.findClosestPoint(modelPoint).id
-      (newId, targetPoint, uncertainty)
-    })
-    (marginalizedModel, newCorrespondences)
+    override def logValue(sample: Sample): Double = {
+
+      val currModelInstance = model.instance(sample.parameters.modelCoefficients).transform(sample
+        .poseTransformation)
+
+      val likelihoods = correspondences.map(correspondence => {
+        val (id, targetPoint, uncertainty) = correspondence
+        val modelInstancePoint = currModelInstance.pointSet.point(id)
+        val observedDeformation = targetPoint - modelInstancePoint
+
+        uncertainty.logpdf(observedDeformation.toBreezeVector)
+      })
+
+
+      val loglikelihood = likelihoods.sum
+      loglikelihood
+    }
   }
-}
 
-case class CachedEvaluator[A](evaluator: DistributionEvaluator[A]) extends
-  DistributionEvaluator[A] {
-  val memoizedLogValue = Memoize(evaluator.logValue, 10)
+  case class CorrespondenceEvaluator(model: StatisticalMeshModel, correspondences: Seq[(PointId,
+    Point3D, MultivariateNormalDistribution)]) extends DistributionEvaluator[Sample] {
 
-  override def logValue(sample: A): Double = {
-    memoizedLogValue(sample)
+    val (marginalizedModel, newCorrespondences) = marginalizeModelForCorrespondences(model,
+      correspondences)
+
+    override def logValue(sample: Sample): Double = {
+
+      val currModelInstance = marginalizedModel.instance(sample.parameters.modelCoefficients)
+        .transform(sample.poseTransformation)
+
+      val likelihoods = newCorrespondences.map(correspondence => {
+        val (id, targetPoint, uncertainty) = correspondence
+        val modelInstancePoint = currModelInstance.pointSet.point(id)
+        val observedDeformation = targetPoint - modelInstancePoint
+
+        uncertainty.logpdf(observedDeformation.toBreezeVector)
+      })
+
+
+      val loglikelihood = likelihoods.sum
+      loglikelihood
+    }
+
+    def marginalizeModelForCorrespondences(model: StatisticalMeshModel, correspondences: Seq[
+      (PointId, Point3D, MultivariateNormalDistribution)]): (StatisticalMeshModel, Seq[(PointId,
+      Point3D, MultivariateNormalDistribution)]) = {
+
+      val (modelIds, _, _) = correspondences.unzip3
+      val marginalizedModel = model.marginal(modelIds.toIndexedSeq)
+      val newCorrespondences = correspondences.map(idWithTargetPoint => {
+        val (id, targetPoint, uncertainty) = idWithTargetPoint
+        val modelPoint = model.referenceMesh.pointSet.point(id)
+        val newId = marginalizedModel.referenceMesh.pointSet.findClosestPoint(modelPoint).id
+        (newId, targetPoint, uncertainty)
+      })
+      (marginalizedModel, newCorrespondences)
+    }
   }
-}
+
+  case class CachedEvaluator[A](evaluator: DistributionEvaluator[A]) extends
+    DistributionEvaluator[A] {
+    val memoizedLogValue = Memoize(evaluator.logValue, 10)
+
+    override def logValue(sample: A): Double = {
+      memoizedLogValue(sample)
+    }
+  }
 
